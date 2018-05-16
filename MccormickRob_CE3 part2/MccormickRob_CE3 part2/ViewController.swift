806ff9fe333e5a4d0debc210cd1285b4be87a2a1
ilivenicely <remccormick@fullsail.edu>
2018-05-17 02:42:31 +0800

    Add files via upload

---
+++ MccormickRob_CE3 part2/MccormickRob_CE3 part2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MccormickRob_CE3 part2
+//
+//  Created by Robert  McCormick on 10/01/2018.
+//  Copyright © 2018 Robert  McCormick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
